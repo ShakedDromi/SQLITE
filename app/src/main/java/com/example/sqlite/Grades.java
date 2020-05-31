621af6fdc30bc945e07e317705458d07c219f481
ShskedDromi <sd5308@bs.amalnet.k12.il>
2020-05-31 23:36:53 +0300

    fst

---
+++ app/src/main/java/com/example/sqlite/Grades.java
@@ -0,0 +1,89 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class Grades extends AppCompatActivity {
+
+    SQLiteDatabase db;
+    com.example.sqlite.HelperDB hlp;
+    EditText etNAME, etQUAR, etGRADE;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grades);
+
+        etNAME = (EditText) findViewById(R.id.etNAME);
+        etQUAR = (EditText) findViewById(R.id.etQUAR);
+        etGRADE = (EditText) findViewById(R.id.etGRADE);
+
+        hlp = new com.example.sqlite.HelperDB(this);
+        db = hlp.getWritableDatabase();
+        db.close();
+    }
+
+    /**
+     * put in variables the edittext input
+     * put in content value the field name and the value
+     * @param view
+     */
+    public void entergrades(View view) {
+
+        String name, strQuar, strGrade;
+        int Grade, Quar;
+        name = etNAME.getText().toString();
+        strQuar = etQUAR.getText().toString();
+        Quar = Integer.parseInt(strQuar);
+        strGrade = etGRADE.getText().toString();
+        Grade = Integer.parseInt(strGrade);
+
+        ContentValues cv = new ContentValues();
+        cv.put(com.example.sqlite.GradesC.NAMES,name);
+        cv.put(com.example.sqlite.GradesC.QUARTER, Quar);
+        cv.put(com.example.sqlite.GradesC.GRADE, Grade);
+
+        db = hlp.getWritableDatabase();
+        db.insert(com.example.sqlite.GradesC.TABLE_GRADES, null, cv);
+        db.close();
+
+    }
+    /**
+     * creates options menu that moves to other activities
+     * @param menu
+     * @return
+     */
+
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected (MenuItem item){
+        String st = item.getTitle().toString();
+        if (st.equals("Enter Data")) {
+            Intent si = new Intent(this, MainActivity.class);
+            startActivity(si);
+        }
+        if (st.equals("Info")){
+            Intent si = new Intent(this, Info.class);
+            startActivity(si);
+        }
+        if(st.equals("Grades")){
+            Intent si = new Intent(this, Grades.class);
+            startActivity(si);
+        }
+        if (st.equals("Sort")){
+            Intent si = new Intent(this, Sort.class);
+            startActivity(si);
+        }
+        return true;
+    }
+}
